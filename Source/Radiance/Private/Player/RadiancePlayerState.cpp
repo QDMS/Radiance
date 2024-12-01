ad0a22fc16df6d93eca57a5ba7ceb2946dbb6765
Qujuan Miller <qujuanmiller@gmail.com>
2024-12-01 14:57:13 -0500

    Created PlayerState

---
+++ Source/Radiance/Private/Player/RadiancePlayerState.cpp
@@ -0,0 +1,10 @@
+// Copyright TPGames
+
+
+#include "Player/RadiancePlayerState.h"
+
+ARadiancePlayerState::ARadiancePlayerState()
+{
+	// Use the setter method to configure the net update frequency
+	SetNetUpdateFrequency(100.f);
+}
---
+++ Source/Radiance/Public/Player/RadiancePlayerState.h
@@ -0,0 +1,18 @@
+// Copyright TPGames
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "RadiancePlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RADIANCE_API ARadiancePlayerState : public APlayerState
+{
+	GENERATED_BODY()
+public:
+	ARadiancePlayerState();
+};
