2998e71026efd245ef5eb9129401f1f2c81d1b13
Qujuan Miller <qujuanmiller@gmail.com>
2024-12-05 15:10:30 -0500

    Effect Stacking

--- Source/Radiance/Private/Actor/RadianceEffectActor.cpp
+++ Source/Radiance/Private/Actor/RadianceEffectActor.cpp
@@ -2,52 +2,34 @@
 
 
 #include "Actor/RadianceEffectActor.h"
-
+#include "AbilitySystemBlueprintLibrary.h"
 #include "AbilitySystemComponent.h"
-#include "AbilitySystemInterface.h"
-#include "AbilitySystem/RadianceAttributeSet.h"
-#include "Components/SphereComponent.h"
-
 
 ARadianceEffectActor::ARadianceEffectActor()
 {
  
 	PrimaryActorTick.bCanEverTick = false;
 
-	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
-	SetRootComponent(Mesh);
-
-	Sphere = CreateDefaultSubobject<USphereComponent>("Sphere");
-	Sphere->SetupAttachment(GetRootComponent());
-	
-
+	SetRootComponent(CreateDefaultSubobject<USceneComponent>("SceneRoot"));
 }
-
-void ARadianceEffectActor::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
-	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
-{
-	//TODO: Change this to apply a Gameplay Effect. For now, using const_cast as a hack!
-	if (IAbilitySystemInterface* ASCInterface = Cast<IAbilitySystemInterface>(OtherActor))
-	{
-		const URadianceAttributeSet* RadianceAttributeSet = Cast<URadianceAttributeSet>(ASCInterface->GetAbilitySystemComponent()->GetAttributeSet(URadianceAttributeSet::StaticClass()));
-		URadianceAttributeSet* MutableRadianceAttributeSet = const_cast<URadianceAttributeSet*>(RadianceAttributeSet);
-		MutableRadianceAttributeSet->SetHealth(RadianceAttributeSet->GetHealth() + 25.f);
-		MutableRadianceAttributeSet->SetMana(RadianceAttributeSet->GetMana() - 25.f);
-		Destroy();
-	}
-	
-}
-
-void ARadianceEffectActor::EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
-	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
-{
-}
+ 
 
 void ARadianceEffectActor::BeginPlay()
 {
 	Super::BeginPlay(); 
 
-	Sphere->OnComponentBeginOverlap.AddDynamic(this, &ARadianceEffectActor::OnOverlap);
-	Sphere->OnComponentEndOverlap.AddDynamic(this, &ARadianceEffectActor::EndOverlap);
+	
+}
+
+void ARadianceEffectActor::ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGameplayEffect> GameplayEffectClass)
+{
+   UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor);
+   if (TargetASC == nullptr) return;
+
+	check(GameplayEffectClass);
+	FGameplayEffectContextHandle EffectContextHandle = TargetASC->MakeEffectContext();
+	EffectContextHandle.AddSourceObject(this);
+	FGameplayEffectSpecHandle EffectSpec = TargetASC->MakeOutgoingSpec(GameplayEffectClass, 1.f, EffectContextHandle);
+	TargetASC->ApplyGameplayEffectSpecToSelf(*EffectSpec.Data.Get());
 }
 
--- Source/Radiance/Public/Actor/RadianceEffectActor.h
+++ Source/Radiance/Public/Actor/RadianceEffectActor.h
@@ -6,7 +6,7 @@
 #include "GameFramework/Actor.h"
 #include "RadianceEffectActor.generated.h"
 
-class USphereComponent;
+class UGameplayEffect;
 
 UCLASS()
 class RADIANCE_API ARadianceEffectActor : public AActor
@@ -17,18 +17,20 @@ public:
 
 	ARadianceEffectActor();
 
-	UFUNCTION()
-	virtual void OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
-	UFUNCTION()
-	virtual void EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
 protected:
 	
 	virtual void BeginPlay() override;
 
-private:
-	UPROPERTY(VisibleAnywhere)
-	TObjectPtr<USphereComponent> Sphere;
+	UFUNCTION(BlueprintCallable)
+	void ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGameplayEffect> GameplayEffectClass);
 
-	UPROPERTY(VisibleAnywhere)
-	TObjectPtr<UStaticMeshComponent> Mesh;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Appied Effects")
+	TSubclassOf<UGameplayEffect> InstantGameplayEffectClass;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Appied Effects") 
+	TSubclassOf<UGameplayEffect> DurationGameplayEffectClass;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Appied Effects") 
+	TSubclassOf<UGameplayEffect> InfiniteGameplayEffectClass; 
 };
